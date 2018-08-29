f6978d6e3cdd8b554c65da2f82685f6995022585
DivaAduriMaharani <divareyy@gmail.com>
2018-08-29 15:56:46 +0700

    first

---
+++ App.js
@@ -0,0 +1,62 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Image
+} from 'react-native';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+  android: 'Kelas : XI RPL 4\n' +
+    'No Absen : 10',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Hello!
+        </Text>
+        <Text style={styles.instructions}>
+          Nama : Diva Audri Maharani
+        </Text>
+        <Text style={styles.instructions}>
+          {instructions}
+        </Text>
+          <Image style={{width : 400, height : 400}}
+          source={require('./divau.jpg')}
+          />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
