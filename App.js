306ca4df5a7fffdd73cf9b6a16f1f95dde294299
DivaAduriMaharani <divareyy@gmail.com>
2018-08-30 19:15:50 +0700

    reactnative3

--- App.js
+++ App.js
@@ -1,16 +1,18 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, AppRegistry,Image} from 'react-native';
+import {Platform, StyleSheet, Text, View, AppRegistry, Image} from 'react-native';
+import Judul from './Components/Judul';
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
+		<Judul/>
         <Text style={styles.judul}>Hello !</Text>
         <Text style={styles.tulis}>Nama : Diva Audri Maharani</Text>
 		    <Text style={styles.tulis}>No : 10</Text>
 		    <Text style={styles.tulis}>Kelas : XI RPL 4</Text>
-        <Image source={require('./divau.jpg')} style={{width: 340, height: 433}}/>
+		    <Image source={require('./divau.jpg')} style={{width: 340, height: 433}}/>
       </View>
     );
   }
@@ -23,11 +25,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-    judul: {
-  fontSize: 20,
+  judul: {
+    fontSize: 18,
     textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    margin: 10,
   },
   tulis: {
 	fontSize: 20,
